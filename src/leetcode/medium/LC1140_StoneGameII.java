be4932679d2b134e1bff09d26cbd8f50f7f484b1
NinadNema <ninad2800@gmail.com>
2026-08-09 15:28:16 +0530

    Add Leetcode 1140: Stone Game 2

---
+++ src/leetcode/medium/LC1140_StoneGameII.java
@@ -0,0 +1,59 @@
+package leetcode.medium;
+
+public class LC1140_StoneGameII {
+    public static void main() {
+        LC1140_StoneGameII lc = new LC1140_StoneGameII();
+
+        int[] arr = {2,7,9,4,4};
+        System.out.println(lc.stoneGameII(arr));
+    }
+
+//  Time Complexity - O(n^2)
+    public int stoneGameII(int[] piles){
+        int n = piles.length;
+
+        int[] suffix = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        int[][] dp = new int[n][n + 1];
+
+        return solve(0, 1, piles, suffix, dp);
+    }
+
+    static int solve(int i, int M, int[] piles, int[] suffix, int[][] dp) {
+
+        int n = piles.length;
+
+        if (i >= n) {
+            return 0;
+        }
+
+        if (2 * M >= n - i) {
+            return suffix[i];
+        }
+
+        if (dp[i][M] != 0) {
+            return dp[i][M];
+        }
+
+        int best = 0;
+
+        for (int X = 1; X <= 2 * M && i + X <= n; X++) {
+
+            int newM = Math.max(M, X);
+
+            int opponent = solve(i + X, newM, piles, suffix, dp);
+
+            int currentPlayer = suffix[i] - opponent;
+
+            best = Math.max(best, currentPlayer);
+        }
+
+        dp[i][M] = best;
+
+        return best;
+    }
+}
