cbb461542379252936de599831386f58eb13fc75
NinadNema <ninad2800@gmail.com>
2026-09-02 18:39:22 +0530

    Add Leetcode 3875: Construct Uniform Parity Array I

---
+++ src/leetcode/easy/LC3875_ConstructUniformParityArrayI.java
@@ -0,0 +1,15 @@
+package leetcode.easy;
+
+public class LC3875_ConstructUniformParityArrayI {
+    public static void main(String[] args) {
+        LC3875_ConstructUniformParityArrayI lc = new LC3875_ConstructUniformParityArrayI();
+
+        int[] nums = {2, 3};
+
+        System.out.println(lc.uniformArray(nums));
+    }
+
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
