a372fbb99176b4573cc809b49dad949f8f619029
NinadNema <ninad2800@gmail.com>
2026-09-17 23:01:38 +0530

    Add Leetcode 101: Symmetric Tree

---
+++ src/leetcode/easy/LC101_SymmetricTree.java
@@ -0,0 +1,49 @@
+package leetcode.easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LC101_SymmetricTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root.left);
+        q.offer(root.right);
+
+        while(!q.isEmpty()){
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+
+            if(left == null && right == null){
+                continue;
+            }
+
+            if(left == null || right == null){
+                return false;
+            }
+
+            if(left.val != right.val){
+                return false;
+            }
+
+            q.offer(left.left);
+            q.offer(right.right);
+            q.offer(left.right);
+            q.offer(right.left);
+        }
+
+        return true;
+    }
+}
