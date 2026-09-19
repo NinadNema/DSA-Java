5f2953c687a57d4167c3ef4d40587ab1da2e5882
NinadNema <ninad2800@gmail.com>
2026-09-19 14:57:02 +0530

    Add Leetcode 226: Invert Binary Tree

---
+++ src/leetcode/easy/LC226_InvertBinaryTree.java
@@ -0,0 +1,34 @@
+package leetcode.easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LC226_InvertBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+//  Time Complexity - O(n)
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
