2b8e16321c81cea9028916a3a2307b6754841d3a
NinadNema <ninad2800@gmail.com>
2026-09-26 23:04:19 +0530

    Add Leetcode 189: Rotate Array

---
+++ src/leetcode/medium/LC189_RotateArray.java
@@ -0,0 +1,35 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+public class LC189_RotateArray {
+    public static void main(String[] args) {
+        LC189_RotateArray lc = new LC189_RotateArray();
+
+        int[] nums = {1,2};
+        int k = 0;
+
+        lc.rotate(nums, k);
+    }
+
+    public void rotate(int[] nums, int k){
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    void reverse(int[] nums,int l, int r){
+        while(l < r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
